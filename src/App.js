787739722ae3ef78208290bf1237c60cdde753a7
Boris <hermosokawai@gmail.com>
2023-09-11 20:27:25 -0500

    subida al cloud 0.1.1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import './App.css';
+import {Navigate, Route, Routes} from "react-router-dom";
+import Dashboard from "./layouts/dashboard";
+
 
 function App() {
     const ambiente = process.env.REACT_APP_AMBIENTE;
 
     return (
-        <>
-            <h1>hola mundo del ambiente de:  {ambiente}</h1>
-        </>
+        <Routes>
+            <Route path="/dashboard/*" element={<Dashboard/>}/>
+            {/*    <Route path="/auth/*" element={<Auth/>}/>*/}
+            <Route path="*" element={<Navigate to="/dashboard/home" replace/>}/>
+        </Routes>
+
+        // <>
+        //
+        //     {/*<DashboardNavbar/>*/}
+        //
+        //     <Navbar routes={['1','2']}/>
+        //
+        //
+        //
+        //     <h1>hola mundo del ambiente de: {ambiente}</h1>
+        //
+        //     <h1 className=" text-3xl font-bold underline">
+        //         Hello world!
+        //     </h1>
+        //
+        //
+        //
+        //     <Footer/>
+        //
+        // </>
+
+
     );
 }
 
 export default App;
+
+
+/*
+@import url('https://fonts.googleapis.com/css2?family=Lunasima:wght@400;700&family=Roboto:wght@100;300;400;500;700&display=swap');
+
+*{
+  margin: 0;
+  padding: 0;
+  font-family: 'Lunasima', sans-serif;
+}
+
+ */
---
+++ src/data/statistics-cards-data.js
@@ -0,0 +1,90 @@
+import {
+    BanknotesIcon,
+    UserPlusIcon,
+    UserIcon,
+    ChartBarIcon,
+} from "@heroicons/react/24/solid";
+import {fetchData} from "../utils/fetchData";
+
+
+export function statisticsCardsData () {
+
+
+}
+
+export default statisticsCardsData;
+
+
+// return apiData.map((coin) => ({
+//     color: "blue",
+//     icon: BanknotesIcon,
+//     title: coin.title,
+//     value: "$"+coin.current_price,
+//     footer: {
+//         color: "text-green-500",
+//         value: coin.ath_change_percentage+"%",
+//         label: "than last week",
+//     }
+// }))
+
+
+//const data = apiData.read();
+
+
+//console.log(apiData.read()),
+
+
+
+
+
+
+/*
+export const statisticsCardsData = [
+    {
+        color: "blue",
+        icon: BanknotesIcon,
+        title: "Today's Money",
+        value: "$53k",
+        footer: {
+            color: "text-green-500",
+            value: "+55%",
+            label: "than last week",
+        },
+    },
+    {
+        color: "pink",
+        icon: UserIcon,
+        title: "Today's Users",
+        value: "2,300",
+        footer: {
+            color: "text-green-500",
+            value: "+3%",
+            label: "than last month",
+        },
+    },
+    {
+        color: "green",
+        icon: UserPlusIcon,
+        title: "New Clients",
+        value: "3,462",
+        footer: {
+            color: "text-red-500",
+            value: "-2%",
+            label: "than yesterday",
+        },
+    },
+    {
+        color: "green",
+        icon: UserPlusIcon,
+        title: "New Clients",
+        value: "3,462",
+        footer: {
+            color: "text-red-500",
+            value: "-2%",
+            label: "than yesterday",
+        },
+    },
+];
+
+export default statisticsCardsData;
+*/
